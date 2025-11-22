c629b38a396cb94f4ba12269b1ad3b09b61cae85
Erlan <bakytzanoverlan234@gmail.com>
2025-11-23 01:26:17 +0500

    Initial Jarvis project

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const JarvisApp());
+}
+
+class JarvisApp extends StatelessWidget {
+  const JarvisApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Jarvis запущен',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
